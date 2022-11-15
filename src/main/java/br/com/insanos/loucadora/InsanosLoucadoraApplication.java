ef5cbef24993e2a0f68aec8b7aee58bb555fa2f8
Deyson-Trindade <trindadedeyson1@gmail.com>
2022-11-15 00:52:26 -0300

    updating InsanosLoucadoraApplication

--- src/main/java/br/com/insanos/loucadora/InsanosLoucadoraApplication.java
+++ src/main/java/br/com/insanos/loucadora/InsanosLoucadoraApplication.java
@@ -2,8 +2,12 @@ package br.com.insanos.loucadora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
+@EnableReactiveMongoRepositories
+@EnableWebFlux
 public class InsanosLoucadoraApplication {
 
 	public static void main(String[] args) {

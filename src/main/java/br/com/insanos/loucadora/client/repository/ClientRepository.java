039f3080f17c1a3623280a125aff54036f6a479a
Deyson-Trindade <trindadedeyson1@gmail.com>
2022-11-15 15:28:02 -0300

    updating repository

--- src/main/java/br/com/insanos/loucadora/client/repository/ClientRepository.java
+++ src/main/java/br/com/insanos/loucadora/client/repository/ClientRepository.java
@@ -2,6 +2,8 @@ package br.com.insanos.loucadora.client.repository;
 
 import br.com.insanos.loucadora.client.document.ClientDocument;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 public interface ClientRepository extends ReactiveMongoRepository<ClientDocument, String> {
+    Mono<ClientDocument> findByDocumentNumberAndActiveAccountIsTrue(String idClient);
 }

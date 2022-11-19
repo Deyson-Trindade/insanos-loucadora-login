d5c9d06737e1a92d66ee36aa0633bf1c4f8a8c63
Deyson-Trindade <trindadedeyson1@gmail.com>
2022-11-19 01:52:23 -0300

    annotating class with @OpenAPIDefinition

--- src/main/java/br/com/insanos/loucadora/InsanosLoucadoraApplication.java
+++ src/main/java/br/com/insanos/loucadora/InsanosLoucadoraApplication.java
@@ -1,5 +1,7 @@
 package br.com.insanos.loucadora;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
@@ -8,6 +10,7 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 @SpringBootApplication
 @EnableReactiveMongoRepositories
 @EnableWebFlux
+@OpenAPIDefinition(info = @Info(title = "Client Manager", version = "1.0", description = "API responsible to manage the clients accounts."))
 public class InsanosLoucadoraApplication {
 
 	public static void main(String[] args) {

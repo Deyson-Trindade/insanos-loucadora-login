e1792e293a5d2eb067ac7df37eb023c0d0fcd92a
Deyson-Trindade <trindadedeyson1@gmail.com>
2022-11-19 01:53:37 -0300

    updating router class

--- src/main/java/br/com/insanos/loucadora/client/router/ClientRouter.java
+++ src/main/java/br/com/insanos/loucadora/client/router/ClientRouter.java
@@ -1,33 +1,50 @@
 package br.com.insanos.loucadora.client.router;
 
+import br.com.insanos.loucadora.client.document.ClientDocument;
 import br.com.insanos.loucadora.client.handler.ClientHandler;
+import br.com.insanos.loucadora.client.request.ClientRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
 
 @Configuration
+@RequiredArgsConstructor
 public class ClientRouter {
-
     private final ClientHandler handler;
 
-    public ClientRouter(ClientHandler handler) {
-        this.handler = handler;
-    }
 
     @Bean
+    @RouterOperations(
+            {
+                    @RouterOperation(path = "/client"
+                            , produces = {
+                            MediaType.APPLICATION_JSON_VALUE}, method = RequestMethod.POST, beanClass = ClientHandler.class, beanMethod = "createClient",
+                            operation = @Operation(operationId = "creatClient", responses = {
+                                    @ApiResponse(responseCode = "200", description = "successful operation")},
+                                    requestBody = @RequestBody(content = @Content(schema = @Schema(implementation = ClientRequest.class)))
+                            )
+                    )
+            }
+    )
     public RouterFunction<ServerResponse> handler() {
-        return route().path("/client", builder ->
-                        builder.nest(accept(APPLICATION_JSON), routerBuilder -> routerBuilder
-                                //.GET("{name}", handler::greetings)
-                                .GET("{idClient}", handler::getClient)
-                                .POST(handler::createClient)
-                                .DELETE("{idClient}", handler::deleteClient)))
+        return route()
+                .POST("/client", handler::createClient)
+                .GET("/client/{idClient}", handler::getClient)
+                .DELETE("/client/{idClient}", handler::deleteClient)
                 .build();
     }
 

4293240e7e0d3a884989fe32c1d1317e2dc9cbc8
Deyson-Trindade <trindadedeyson1@gmail.com>
2022-11-21 19:42:58 -0300

    creating verifyClientAuthenticy method and encrypting password

--- src/main/java/br/com/insanos/loucadora/client/service/ClientService.java
+++ src/main/java/br/com/insanos/loucadora/client/service/ClientService.java
@@ -4,20 +4,22 @@ import br.com.caelum.stella.validation.CPFValidator;
 import br.com.insanos.loucadora.client.document.ClientDocument;
 import br.com.insanos.loucadora.client.repository.ClientRepository;
 import br.com.insanos.loucadora.client.request.Client;
+import br.com.insanos.loucadora.client.utils.Encryptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ClientService {
-
     private final ClientRepository repository;
-
     private final CPFValidator cpfValidator;
+    private final Encryptor encryptor;
+
 
     public Mono<ClientDocument> createClient(final Client client) {
         cpfValidator.assertValid(client.getDocumentNumber());
@@ -25,7 +27,7 @@ public class ClientService {
                 .id(String.valueOf(UUID.randomUUID()))
                 .documentNumber(client.getDocumentNumber())
                 .name(client.getName())
-                .password(client.getPassword())
+                .password(encryptor.encrypt(client.getPassword()))
                 .activeAccount(true)
                 .createdAt(LocalDateTime.now())
                 .build();
@@ -37,6 +39,13 @@ public class ClientService {
         return repository.findByDocumentNumberAndActiveAccountIsTrue(idClient);
     }
 
+    public Mono<Boolean> verifyClientAuthenticy(final Client client) {
+        return Mono.just(client.getPassword())
+                .map(encryptor::encrypt)
+                .flatMap(encrypted -> repository.findByDocumentNumberAndPasswordAndActiveAccountIsTrue(client.getDocumentNumber(), encrypted))
+                .map(Objects::nonNull);
+    }
+
     public Mono<ClientDocument> inactivateAccount(final String idClient) {
         return repository.findByDocumentNumberAndActiveAccountIsTrue(idClient)
                 .flatMap(account -> {
@@ -44,6 +53,5 @@ public class ClientService {
                     account.setUpdatedAt(LocalDateTime.now());
                     return repository.save(account);
                 });
-
     }
 }

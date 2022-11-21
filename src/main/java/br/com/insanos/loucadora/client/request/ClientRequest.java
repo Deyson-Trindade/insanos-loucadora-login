1affb0316104caf69e8d76d4a66b07c4cd05d82d
Deyson-Trindade <trindadedeyson1@gmail.com>
2022-11-21 10:33:11 -0300

    annotating class with @Builder

--- src/main/java/br/com/insanos/loucadora/client/request/ClientRequest.java
+++ src/main/java/br/com/insanos/loucadora/client/request/ClientRequest.java
@@ -1,12 +1,10 @@
 package br.com.insanos.loucadora.client.request;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class ClientRequest implements Client {
